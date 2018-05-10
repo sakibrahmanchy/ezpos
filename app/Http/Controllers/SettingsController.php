03e5fcc654f45f458ef184c355fabf46a46bd3b8
sakibrahmanchy <sakib.cse11.cuet@gmail.com>
2018-05-10 11:28:31 +0630

    Settings Issue

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -44,7 +44,8 @@ class SettingsController extends Controller
 
         $denomintaionNames = $request->denomination_name;
         $denomintaionValues = $request->denomination_value;
-
+        
+        if(!empty($denomintaionNames))
         for($i = 0; $i<sizeof($denomintaionNames); $i++){
             if(is_null($denomintaionNames[$i]))
                 $denomintaionNames[$i] = "";
